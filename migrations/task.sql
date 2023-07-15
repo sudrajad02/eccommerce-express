536ded55fae35d0746a836e984abaef1c6ccd486
sudrajad02 <anggiperdanasudrajad15@gmail.com>
2023-07-16 02:45:23 +0700

    Remove migration task

--- migrations/task.sql
+++
@@ -1,8 +0,0 @@
-create table tasks
-(
-    id        int auto_increment,
-    name      varchar(255)          null,
-    completed boolean default false null,
-    constraint tasks_pk
-    primary key (id)
-);
